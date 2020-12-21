d68c797506cae03c012cd62ea9de8608f48ad4c2
huangJW7 <1033274019@qq.com>
2020-12-21 16:09:43 +0800

    sql

--- application/test/controller/Hello.php
+++ application/test/controller/Hello.php
@@ -1,11 +1,19 @@
 <?php
 namespace app\test\controller;
+use app\admin\model\Config;
 use think\Controller;
 
 class Hello extends Controller{
-    public function eat(){
-        $name = 'test23';
-        return 'first'.$name;
+    public function addfile(){
+        $data = new Config();
+        $data ->default = 4;
+        $data ->tomorrow = 1;
+        $data ->isset = 1;
+        print_r($data);
+
+        $data ->save();
+        if($data ===true)
+            echo "add ok";
 
     }
 }
