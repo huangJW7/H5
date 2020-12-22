8ad7fb59d36c6c0e7d3f44b7ed0d4930045e1680
huangJW7 <1033274019@qq.com>
2020-12-22 21:28:24 +0800

    sql

--- application/admin/controller/Pass.php
+++ application/admin/controller/Pass.php
@@ -32,32 +32,30 @@ class Pass extends Controller{
 
         //待添加登录逻辑
         $type = Request::param('type');
-        if($type!= 1 || $type!=0 ){
-            return msg (-1,'wrong type');
-        }
-        $query1 = ShowerMsg::where('pass',0)->where('type',$type);
-        $query2 = ShowerMsg::getOpenData($query1);
-        $query3 = ShowerMsg::getPrivateData($query2);
-        $datas =$query3->select();
+        if($type <=1 ) {
 
-        return msg (0,'ok',$datas);
+            $query1 = ShowerMsg::where('pass', 0)->where('type', $type);
+            $query2 = ShowerMsg::getOpenData($query1);
+            $query3 = ShowerMsg::getPrivateData($query2);
+            $datas = $query3->select();
+            return msg(0, 'ok', $datas);
+        }
     }
     public function check_image(){
         //返回待审核的图片列表
         $type = Request::param('type');
-        if($type != 1 || $type!=0){
-            return msg (-1,'wrong type');
-        }
+        if($type <=1) {
 
-        $IDs = ShowerMsg::field('ID')->where('pass',0)->where('type',$type)->select();
-        $count = 0;
-        foreach ($IDs as $ID){
-            $data[$count][ID] = $ID;
-            $data[$count][image] = Picture::where('ID',$ID)->where('type','普通')->select();
-            $count+=1;
+            $IDs = ShowerMsg::field('ID')->where('pass', 0)->where('type', $type)->select();
+            $count = 0;
+            foreach ($IDs as $ID) {
+                $data[$count][ID] = $ID;
+                $data[$count][image] = Picture::where('ID', $ID)->where('type', '普通')->select();
+                $count += 1;
 
+            }
+            return msg(0, 'ok', $data);
         }
-        return msg(0,'ok',$data);
 
     }
 
