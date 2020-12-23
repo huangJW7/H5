27d27b28deb088ec2153a803c69ed8c040b70b0f
huangJW7 <1033274019@qq.com>
2020-12-23 14:59:56 +0800

    sql

--- application/admin/controller/Pass.php
+++ application/admin/controller/Pass.php
@@ -57,7 +57,7 @@ class Pass extends Controller{
                 foreach ($data[$count1]['image'] as $key => $vaule){
                     //vaule ="{\"address\":\"20201222\\/07316443315b68108d9f7d1299f88777.png\"}
                     $vaule = json_decode($vaule,true);
-                    $data[$count1]['image'][$key] = PREFIX.$vaule;
+                    $data[$count1]['image'][$key] = PREFIX.$vaule['address'];
                     $count2+=1;
                 }
                 $count1 += 1;
