3aea7abc296b6325cf66bb8786afb1f2d14d87d8
huangJW7 <1033274019@qq.com>
2021-01-01 22:25:50 +0800

    sql

--- public/router.php
+++ public/router.php
@@ -13,5 +13,6 @@
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     return false;
 } else {
-    require __DIR__ . "/index.php";
+    require __DIR__ . "/dist/index.html";
+    //require __DIR__ . "/index.php";
 }
