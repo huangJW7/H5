f32f13b08dab8847e1e99537d5d460dbd3be5b22
huangJW7 <1033274019@qq.com>
2021-01-15 23:31:48 +0800

    sql

--- application/test/controller/Hello.php
+++ application/test/controller/Hello.php
@@ -4,23 +4,12 @@ use app\test\model\Config;
 use think\Controller;
 
 class Hello extends Controller{
-    public function addfile(){
-        $data = new Config();
-        $data ->default = 4;
-        $data ->tomorrow = 1;
-        $data ->isset = 1;
-        print_r($data);
 
-        $data ->save();
-        if($data ===true)
-            echo "add ok";
-
-    }
     public function showtest(){
         $default = null;
-        $data = Config::where('default',$default)->where('tomorrow',2)->fetchSql(true)->find();
+        $data = Config::limit(1)->find();
         if(empty($data))
-            return;
-
+            return msg(-1,'empty data');
+        return msg(0,'ok',$data);
     }
 }
