9f829f9e978f2079f9abf9f3cf131cf9ccd8f4a7
huangJW7 <1033274019@qq.com>
2021-04-04 19:54:07 +0800

    sql

--- application/admin/controller/Command.php
+++ application/admin/controller/Command.php
@@ -602,7 +602,10 @@ class Command extends Controller{
 
         $o = new \SaeTClientV2('3190024882' , '747c0c57d6e943ddeff70f496a2b9544' , $access_token);
 
-        $text ="测试   换行了吗  这次呢? http://www.scgxtd.cn/public/dist/img/qrcode.6f760a25.png";
+        $text ="测试
+        换行了吗  
+        这次呢? 
+        http://www.scgxtd.cn/public/dist/img/qrcode.6f760a25.png";
         $app_img_file = __DIR__.'/../../../public/public/picture/20210404/042acc16c4234f2f5b45dd4411bd4ed3.png';
         if (file_exists($app_img_file)) {
             $fp = fopen($app_img_file, "r");
