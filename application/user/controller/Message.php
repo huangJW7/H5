50c24d393086cd42422e9911357e3d1bcd20350e
huangJW7 <1033274019@qq.com>
2021-04-26 23:21:02 +0800

    sql

--- application/user/controller/Message.php
+++ application/user/controller/Message.php
@@ -148,13 +148,21 @@ class Message extends Controller{
                 if ($data->like >= $like && is_numeric($data->history)) {
                     $text = "【" . $config_data->number . "号" . $data->gender . "神】 第" . $data->history . "期 [心]蹲评论区[心]
 " . $data->name . " " . $data->height . " " . $data->gender . " " . $data->age . " " . $data->star . " " . $data->school . " " . $data->background . "
-我的日常：" . $data->introduction . "http://www.scgxtd.cn/public/dist/img/qrcode.e31cac66.png";
+我的日常：" . $data->introduction;
+                    rebuild:
+                    $add_address_text = $text."http://www.scgxtd.cn/public/dist/img/qrcode.e31cac66.png";
                     $pic_address = Picture::limit(1)->where('ID', $data->ID)->where('type', 0)->column('address');
                     $content1 = 'http://www.scgxtd.cn/public/public/picture/' . $pic_address[0];
                     $o = new \SaeTClientV2('3190024882', '747c0c57d6e943ddeff70f496a2b9544', $config_data->token);
-                    $post_text = urlencode($text);
+                    $post_text = urlencode($add_address_text);
                     $ret = $o->share($post_text, $content1);    //发送微博
                     if (isset($ret['error_code']) && $ret['error_code'] > 0) {
+                        //使用goto 减少10个字符，再次发送
+                        if($ret['error_code']==20012){
+                            $text = substr($text,0,strlen($text)-10);
+                            goto rebuild;
+                        }
+
                     } else {
                         $save = new Posted();
                         $save->openid = $actorUID;
@@ -187,15 +195,20 @@ class Message extends Controller{
                 if ($data->like >= $like && is_numeric($data->history)) {
                     $text = "【" . $config_data->number . "号" . $data->gender . "神】 第" . $data->history . "期 [心]蹲评论区[心]
 " . $data->name . " " . $data->height . " " . $data->gender . " " . $data->age . " " . $data->star . " " . $data->school . " " . $data->background . "
-我的日常：" . $data->introduction . "http://www.scgxtd.cn/public/dist/img/qrcode.e31cac66.png";
+我的日常：" . $data->introduction;
+                    rebuild:
+                    $add_address_text = $text."http://www.scgxtd.cn/public/dist/img/qrcode.e31cac66.png";
                     $pic_address = Picture::limit(1)->where('ID', $data->ID)->where('type', 0)->column('address');
                     $content1 = 'http://www.scgxtd.cn/public/public/picture/' . $pic_address[0];
                     $o = new \SaeTClientV2('3190024882', '747c0c57d6e943ddeff70f496a2b9544', $config_data->token);
-                    $post_text = urlencode($text);
+                    $post_text = urlencode($add_address_text);
                     $ret = $o->share($post_text, $content1);    //发送微博
                     if (isset($ret['error_code']) && $ret['error_code'] > 0) {
-                        echo "<p>发送失败，错误：{$ret['error_code']}:{$ret['error']}</p>";
-                        echo $text;
+                        //使用goto 减少10个字符，再次发送
+                        if($ret['error_code']==20012){
+                            $text = substr($text,0,strlen($text)-10);
+                            goto rebuild;
+                        }
                     } else {
                         $save = new Posted();
                         $save->openid = $actorUID;
